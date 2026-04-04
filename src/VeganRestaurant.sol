876dffb79d98e66e287a172f17773f84b0e6ec0f
jprecreativo <jprecreativo@gmail.com>
2026-04-04 13:55:04 +0200

    Rename script and test files and add new functions in the contract

--- src/VeganRestaurant.sol
+++ src/VeganRestaurant.sol
@@ -12,10 +12,32 @@ contract VeganRestaurant {
     // Mapping from the Open Location Code (Plus Code) to its details.
     mapping(string => Restaurant) public restaurants;
 
-    function addRestaurant(string memory plusCode) external {
+    modifier onlyOwner(string memory plusCode) {
+        require(restaurants[plusCode].owner == msg.sender, "Only the owner can perform this action.");
+        
+        _;
+    }
+
+    function addRestaurant(string memory plusCode) public {
         require(bytes(plusCode).length > 0, "Plus code cannot be empty.");
         require(restaurants[plusCode].owner == address(0), "Restaurant already exists.");
 
         restaurants[plusCode] = Restaurant({owner: msg.sender, avgRating: 0, totalRatings: 0});
+    }
+    function deleteRestaurant(string memory plusCode) public onlyOwner(plusCode) {
+        delete restaurants[plusCode];
+    }
+    function modifyRestaurant(string memory plusCode, string memory newPlusCode) external onlyOwner(plusCode) {
+        require(bytes(plusCode).length > 0, "Plus code cannot be empty.");
+        require(restaurants[plusCode].owner != address(0), "Restaurant does not exist.");
+
+        uint currentAvgRating = restaurants[plusCode].avgRating;
+        uint currentTotalRatings = restaurants[plusCode].totalRatings;
+
+        deleteRestaurant(plusCode);
+        addRestaurant(newPlusCode);
+        
+        restaurants[newPlusCode].avgRating = currentAvgRating;
+        restaurants[newPlusCode].totalRatings = currentTotalRatings;
     }
 }
