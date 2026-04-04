b5330834f8efeddc8e1f7b4a9dfcaa255eb3e52f
jprecreativo <jprecreativo@gmail.com>
2026-04-04 12:58:16 +0200

    Rename 'Counter.sol' to 'VeganResturant.sol' and add the first implementation of the contract

---
+++ src/VeganRestaurant.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract VeganRestaurant {
+    struct Restaurant {
+        address owner;
+        uint avgRating;
+        uint totalRatings;
+    }
+    // Version number for the contract.
+    uint public version = 1;
+    // Mapping from the Open Location Code (Plus Code) to its details.
+    mapping(string => Restaurant) public restaurants;
+
+    function addRestaurant(string memory plusCode) external {
+        require(bytes(plusCode).length > 0, "Plus code cannot be empty.");
+        require(restaurants[plusCode].owner == address(0), "Restaurant already exists.");
+
+        restaurants[plusCode] = Restaurant({
+            owner: msg.sender,
+            avgRating: 0,
+            totalRatings: 0
+        });
+    }
+}
