87eaa6e1d7692b35ae3a1f049e370c5dc1f79171
jprecreativo <jprecreativo@gmail.com>
2026-04-05 12:06:42 +0200

    Use OpenZepellin in the contract

--- src/VeganRestaurant.sol
+++ src/VeganRestaurant.sol
@@ -1,20 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.29;
 
-contract VeganRestaurant {
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract VeganRestaurant is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     struct Restaurant {
         address owner;
         uint256 avgRating;
         uint256 totalRatings;
     }
+
     // Version number for the contract.
-    uint256 public version = 1;
+    uint256 public version;
+
     // Mapping from the Open Location Code (Plus Code) to its details.
     mapping(string => Restaurant) public restaurants;
 
-    modifier onlyOwner(string memory plusCode) {
-        require(restaurants[plusCode].owner == msg.sender, "Only the owner can perform this action.");
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
+        version = 1;
+    }
 
+    modifier onlyRestaurantOwner(string memory plusCode) {
+        require(restaurants[plusCode].owner == msg.sender, "Only the owner can perform this action.");
         _;
     }
 
@@ -25,11 +35,14 @@ contract VeganRestaurant {
         restaurants[plusCode] = Restaurant({owner: msg.sender, avgRating: 0, totalRatings: 0});
     }
 
-    function deleteRestaurant(string memory plusCode) public onlyOwner(plusCode) {
+    function deleteRestaurant(string memory plusCode) public onlyRestaurantOwner(plusCode) {
         delete restaurants[plusCode];
     }
 
-    function modifyRestaurant(string memory plusCode, string memory newPlusCode) external onlyOwner(plusCode) {
+    function modifyRestaurant(string memory plusCode, string memory newPlusCode)
+        external
+        onlyRestaurantOwner(plusCode)
+    {
         require(bytes(plusCode).length > 0, "Plus code cannot be empty.");
         require(restaurants[plusCode].owner != address(0), "Restaurant does not exist.");
 
@@ -42,4 +55,7 @@ contract VeganRestaurant {
         restaurants[newPlusCode].avgRating = currentAvgRating;
         restaurants[newPlusCode].totalRatings = currentTotalRatings;
     }
+
+    // This restricts contract upgrades to the global owner of the contract.
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
