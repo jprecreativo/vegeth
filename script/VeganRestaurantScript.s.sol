70fbcba25fb1554a821249ee534ec4d0981fbdeb
jprecreativo <jprecreativo@gmail.com>
2026-04-13 12:31:22 +0200

    Add to the deployment script the version 2 of the contract

--- script/VeganRestaurantScript.s.sol
+++ script/VeganRestaurantScript.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.29;
 
 import {Script, console} from "forge-std/Script.sol";
 import {VeganRestaurant} from "../src/VeganRestaurant.sol";
+import {VeganRestaurant_v2} from "../src/VeganRestaurant_v2.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract VeganRestaurantScript is Script {
@@ -12,6 +13,10 @@ contract VeganRestaurantScript is Script {
 
     function run() public {
         vm.startBroadcast();
+
+        // ---------------------------------------------
+        // 1. DEPLOY V1
+        // ---------------------------------------------
 
         // Deploy the v1 implementation.
         VeganRestaurant implementation = new VeganRestaurant();
@@ -34,11 +39,29 @@ contract VeganRestaurantScript is Script {
 
         restaurantProxy.addRestaurant("344FWHMC+24F");
 
-        (address owner, uint256 avgRating, uint256 totalRatings) = restaurantProxy.restaurants("344FWHMC+24F");
+        (address owner,,) = restaurantProxy.restaurants("344FWHMC+24F");
 
-        console.log("Owner:", owner);
-        console.log("Average Rating:", avgRating);
-        console.log("Total Ratings:", totalRatings);
+        console.log("[v1] Owner:", owner);
+
+        // ---------------------------------------------
+        // 2. UPGRADE TO V2
+        // ---------------------------------------------
+
+        // Deploy the new V2 implementation.
+        VeganRestaurant_v2 implementation_v2 = new VeganRestaurant_v2();
+
+        // Call the upgrade function via the proxy.
+        // Since restaurantProxy is an Ownable UUPS contract, only the owner can do this.
+        restaurantProxy.upgradeToAndCall(address(implementation_v2), "");
+
+        // Cast the proxy to the new V2 interface.
+        VeganRestaurant_v2 restaurantProxy_v2 = VeganRestaurant_v2(address(proxy));
+
+        restaurantProxy_v2.addRestaurant("8FVC9G8F+6X");
+
+        (address owner_v2,,) = restaurantProxy_v2.restaurants("8FVC9G8F+6X");
+
+        console.log("[v2] Owner:", owner_v2);
 
         vm.stopBroadcast();
     }
